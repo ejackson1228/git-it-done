90ebeb81295addd4af8f447e0ddd52363544040b
ejackson1228 <ejackson1228@gmail.com>
2022-08-15 18:47:22 -0400

    resolve conflict

--- Assets/js/homepage.js
+++ Assets/js/homepage.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 var getUserRepos = function() {
     fetch("https://api.github.com/users/octocat/repos");
   };
   
     getUserRepos();
-=======
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
-  
-    getUserRepos();
->>>>>>> develop
